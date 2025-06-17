b6fb754f351e30d552171e3839f99d7089c3912d
Patrick Robinson <patrob@gmail.com>
2025-06-16 20:56:52 -0500

    Add Google provider to NextAuth and login UI (#80)

---
+++ app/lib/auth.ts
@@ -0,0 +1,17 @@
+import type { NextAuthOptions } from 'next-auth'
+import GitHubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID ?? '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+}
