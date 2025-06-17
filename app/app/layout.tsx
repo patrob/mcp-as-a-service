31747ab3f832b00d97b0b6056a57511cb0c8b753
Patrick Robinson <patrob@gmail.com>
2025-06-17 06:43:50 -0500

    Ticket #123 fix build by removing google font and updating stripe api handler

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,9 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import AuthProvider from '@/components/session-provider';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
